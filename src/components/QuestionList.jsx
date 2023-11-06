4271ed3734baa6e8c7622f8077f7357fc7e9243a
Matthieu Lepinay <lepinay.matthieu@gmail.com>
2023-11-06 09:53:51 +0400

    Initial commit

---
+++ src/components/QuestionList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { QuestionCard } from "./QuestionCard";
+
+export const QuestionList = () => {
+  // TODO: Add work here
+
+  return (
+    <>
+      <QuestionCard />
+    </>
+  );
+};
